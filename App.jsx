efa6ccf2b099066ad67435dc6bae35f01f74c343
es-tania <tane.stvs@gmail.com>
2022-03-01 17:39:13 +0100

    app: début menu navigation

--- App.jsx
+++ App.jsx
@@ -3,14 +3,18 @@ import { View } from "react-native";
 import { SafeAreaView,StyleSheet } from "react-native";
 import Header from "./components/header";
 import Rdv from "./page/rdv";
+import {NavigationContainer} from '@react-navigation/native';
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
+import Accueil from "./components/accueil";
 
 export default function App() {
+  const Stack = createNativeStackNavigator();
   return (
-    <SafeAreaView style={styles.container}>
-  <Header />
-      <StatusBar />
-      <Rdv />
-    </SafeAreaView>
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="accueil" component={Accueil}/>
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
 const styles = StyleSheet.create({
