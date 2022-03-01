3c54e78004f3c150e5898cde7e418aba8b737c79
DOMESDAY7 <79838340+DOMESDAY7@users.noreply.github.com>
2022-03-01 11:24:38 +0100

    fix

--- App.jsx
+++ App.jsx
@@ -1,12 +1,13 @@
 import { StatusBar } from "expo-status-bar";
 import { View } from "react-native";
 import { SafeAreaView,StyleSheet } from "react-native";
-
+import Header from "./components/header";
 import Rdv from "./page/rdv";
 
 export default function App() {
   return (
     <SafeAreaView style={styles.container}>
+  <Header />
       <StatusBar />
       <Rdv />
     </SafeAreaView>
