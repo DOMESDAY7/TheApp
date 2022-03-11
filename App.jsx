f611e211f07f1dd438af5edc410c04a254c0af43
DOMESDAY7 <79838340+DOMESDAY7@users.noreply.github.com>
2022-03-11 08:53:16 +0100

    fix(App.jsx)

--- App.jsx
+++ App.jsx
@@ -7,12 +7,13 @@ import {NavigationContainer} from '@react-navigation/native';
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
 import Accueil from "./components/accueil";
 
+const Stack = createNativeStackNavigator();
 export default function App() {
-  const Stack = createNativeStackNavigator();
   return (
     <NavigationContainer>
       <Stack.Navigator>
         <Stack.Screen name="accueil" component={Accueil}/>
+        <Stack.Screen name="rendez-vous" component={Rdv}/>
       </Stack.Navigator>
     </NavigationContainer>
   );
--- page/rdv.jsx
+++ page/rdv.jsx
@@ -10,7 +10,7 @@ import {
 import { useState } from "react";
 import DatePicker from "react-native-datepicker";
 
-const Rdv = () => {
+export default function Rdv(){
   const [prenom, setInput] = useState("");
   const [nom, setNom] = useState("");
   const [email, setEmail] = useState("");
