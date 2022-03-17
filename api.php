7e36e4a28adf6fc470d419b8ceb254bb1c6819b0
DOMESDAY7 <79838340+DOMESDAY7@users.noreply.github.com>
2022-03-17 14:16:43 +0100

    feat(Apropos.jsx):get data from api

--- api.php
+++ api.php
@@ -1,14 +1,13 @@
-<?php 
+<?php
 header('Access-Control-Allow-Origin: *'); //Il faudrait mettre que notre site
 header('Content-Type: application/json');
 $db = new PDO('mysql:host=localhost;dbname=wi0d3_ally', 'root', '', array(PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8mb4"));
 
-if(isset($_GET["select"])){
-    $select=$_GET["select"];
-   
-   $sql = "SELECT *  FROM `$select`";
+if (isset($_GET["select"])) {
+    $select = htmlspecialchars($_GET["select"]);
+    //filter_var
+    $sql = "SELECT * FROM $select ";
     $req = $db->query($sql);
-    $data=$req->fetchAll(PDO::FETCH_ASSOC);
-    // var_dump($data);
+    $data = $req->fetchAll(PDO::FETCH_ASSOC);
     echo json_encode($data);
 }
--- page/APropos.jsx
+++ page/APropos.jsx
@@ -1,51 +1,83 @@
-import {
-    StyleSheet,
-    Text,
-    View,
-    SafeAreaView,
-  } from "react-native";
+import { StyleSheet, Text, View, SafeAreaView, FlatList } from "react-native";
 import { useEffect, useState } from "react";
 
-function APropos(){
-    const [dev,setdev]=useState([]);
-    const [social,setSocial]=useState([]);
-    useEffect(()=>{
-        fetch('http://localhost/api.php?select=equipe_dev')
-            .then((res)=>setdev(res.data));
-        fetch('http://localhost/api.php?select=salon')
-            .then((res)=>setSocial(res.data));
-    })
-    const oneDev = (dev) => {
-        <View>
-            <Text>{dev.dev_prenom}</Text>
-            <Text>{dev.dev_nom}</Text>
-            <Text>{dev.dev_github}</Text>
-        </View>
-    }
-    const oneSocialMedia=(social)=>{
-        <View>
-            <Text>{social.facebook}</Text>
-        </View>
-    }
-    return(
-        <SafeAreaView>
-        <Text>A propos du salon</Text>
-        <Text>Dans une ambiance chaleureuse, aux pierres et briques apparentes, nous vous accueillons pour un pur moment de relaxation, et de détente.</Text>
-        <Text>Grâce à la technologie I.C.O.N., nous garantissons des résultats spectaculaires en matière de traitement et de coloration, aussi bien pour les femmes que pour les hommes.</Text>
-        <Text>L'équipe de développeurs</Text>
-        <FlatList
-            data={dev}
-            renderItem={oneDev}
-            keyExtractor={dev => dev.id_dev}
-        />
-        <Text>Nos réseaux</Text>
-        <FlatList
+function APropos() {
+  const [dev, setdev] = useState([]);
+  const [social, setSocial] = useState([]);
+
+  useEffect(() => {
+    //dev
+    fetch("http://172.24.140.187/apiTheApp/index.php?select=equipe_dev")
+      .then((resp) => resp.json())
+      .then((data) => {
+        console.log(data);
+        setdev(
+            data.map((dev) => {
+              return <View style={styles.dev}>
+                  <Text> {dev.dev_prenom} </Text>
+                  <Text> {dev.dev_nom} </Text>
+                  <Text> {dev.github} </Text>
+              </View>;
+            })
+          );
+      });
+
+    //salon
+    fetch("http://172.24.140.187/apiTheApp/index.php?select=salon")
+      .then((resp) => resp.json())
+      .then((data) => {
+        setSocial(data);
+        console.log(data);
+      });
+    
+  }, []);
+  const oneDev = (dev) => {
+    <View>
+      <Text>{dev.dev_prenom}</Text>
+      <Text>{dev.dev_nom}</Text>
+      <Text>{dev.dev_github}</Text>
+    </View>;
+  };
+  const oneSocialMedia = (social) => {
+    <View>
+      <Text>{social.facebook}</Text>
+    </View>;
+  };
+  return (
+    <SafeAreaView>
+      <Text>A propos du salon</Text>
+      <Text>
+        Dans une ambiance chaleureuse, aux pierres et briques apparentes, nous
+        vous accueillons pour un pur moment de relaxation, et de détente.
+      </Text>
+      <Text>
+        Grâce à la technologie I.C.O.N., nous garantissons des résultats
+        spectaculaires en matière de traitement et de coloration, aussi bien
+        pour les femmes que pour les hommes.
+      </Text>
+      <Text>L'équipe de développeurs</Text>
+      <FlatList
+        data={dev}
+        renderItem={oneDev}
+        keyExtractor={(dev) => dev.id_dev}
+      />
+      <Text>Nos réseaux</Text>
+
+      {/* <FlatList
             data={social}
             renderItem={oneSocialMedia}
             keyExtractor={social => social.id_salon}
-        />
-        </SafeAreaView>
-    )
+        /> */}
+      <View >{dev}</View>
+    </SafeAreaView>
+  );
 }
+const styles= StyleSheet.create({
+    allDev:{
+        flex:1,
+        flexDirection:"column"
+    },
+    
+})
 
 export default APropos;
