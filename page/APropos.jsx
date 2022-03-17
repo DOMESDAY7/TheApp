2c2ab897d6ffd8986ea54558e07ff5346d09a16d
es-tania <tane.stvs@gmail.com>
2022-03-17 17:47:57 +0100

    style

--- page/APropos.jsx
+++ page/APropos.jsx
@@ -1,5 +1,5 @@
-import { StyleSheet, Text, View, SafeAreaView, FlatList } from "react-native";
-import { useEffect, useState } from "react";
+import { StyleSheet, Text, View, SafeAreaView, FlatList, TouchableOpacity, Button, Linking } from "react-native";
+import { useEffect, useState, useCallback } from "react";
 
 function APropos() {
   const [dev, setdev] = useState([]);
@@ -7,23 +7,21 @@ function APropos() {
 
   useEffect(() => {
     //dev
-    fetch("http://172.24.140.187/apiTheApp/index.php?select=equipe_dev")
+    fetch("http://172.24.140.219/apiTheApp/index.php?select=equipe_dev")
       .then((resp) => resp.json())
       .then((data) => {
-        console.log(data);
+        // console.log(data);
         setdev(
             data.map((dev) => {
-              return <View style={styles.dev}>
-                  <Text> {dev.dev_prenom} </Text>
-                  <Text> {dev.dev_nom} </Text>
-                  <Text> {dev.github} </Text>
+              return <View style = {styles.dev}>
+                  <Text style = {styles.devinfo}> {dev.dev_prenom} - {dev.dev_nom} - {dev.github}</Text>
               </View>;
             })
           );
       });
 
     //salon
-    fetch("http://172.24.140.187/apiTheApp/index.php?select=salon")
+    fetch("http://172.24.140.219/apiTheApp/index.php?select=salon")
       .then((resp) => resp.json())
       .then((data) => {
         setSocial(data);
@@ -38,45 +36,130 @@ function APropos() {
       <Text>{dev.dev_github}</Text>
     </View>;
   };
-  const oneSocialMedia = (social) => {
-    <View>
-      <Text>{social.facebook}</Text>
-    </View>;
+
+  const supportedURL = "https://www.facebook.com/lesaloniconparis";
+  const OpenURLButton = ({ url, children }) => {
+    const handlePress = useCallback(async () => {
+      // Checking if the link is supported for links with custom URL scheme.
+      const supported = await Linking.canOpenURL(url);
+  
+      if (supported) {
+        // Opening the link with some app, if the URL scheme is "http" the web link should be opened
+        // by some browser in the mobile
+        await Linking.openURL(url);
+      } else {
+        Alert.alert(`Don't know how to open this URL: ${url}`);
+      }
+    }, [url]);
+  
+    return <TouchableOpacity style = {styles.reseau} onPress={handlePress}><Text title={children}>Facebook</Text>
+    </TouchableOpacity>;
   };
+  // const oneSocialMedia = (social) => {
+  //   <View>
+  //     <Text>{social.facebook}</Text>
+  //   </View>;
+  // };
   return (
-    <SafeAreaView>
-      <Text>A propos du salon</Text>
-      <Text>
+    <SafeAreaView style = {styles.container}>
+      <Text style = {styles.header}></Text>
+      <Text style = {styles.titre}>A propos du salon</Text>
+      <Text style = {styles.intro}>
         Dans une ambiance chaleureuse, aux pierres et briques apparentes, nous
         vous accueillons pour un pur moment de relaxation, et de détente.
       </Text>
-      <Text>
+      <Text style = {styles.intro}>
         Grâce à la technologie I.C.O.N., nous garantissons des résultats
         spectaculaires en matière de traitement et de coloration, aussi bien
         pour les femmes que pour les hommes.
       </Text>
-      <Text>L'équipe de développeurs</Text>
-      <FlatList
+      <TouchableOpacity style = {styles.contact}>
+        <Text>Contactez-nous</Text>
+      </TouchableOpacity>
+      <Text style = {styles.soustitre}>L'équipe de développeurs</Text>
+      <View>{dev}</View>
+      {/* <FlatList
         data={dev}
         renderItem={oneDev}
         keyExtractor={(dev) => dev.id_dev}
-      />
-      <Text>Nos réseaux</Text>
+      /> */}
+      <Text style = {styles.soustitre}>Rejoignez-nous !</Text>
+      <OpenURLButton url={supportedURL} style={styles.lien}>Open Supported URL</OpenURLButton>
 
       {/* <FlatList
             data={social}
             renderItem={oneSocialMedia}
             keyExtractor={social => social.id_salon}
         /> */}
-      <View >{dev}</View>
+
     </SafeAreaView>
   );
 }
 const styles= StyleSheet.create({
-    allDev:{
-        flex:1,
-        flexDirection:"column"
-    },
+  container:{
+    flex: 1,
+    backgroundColor: "black",
+    color: "white",
+    padding: 25,
+  },
+  header:{
+    flex:2,
+  },
+  titre:{
+    flex: 1,
+    color:"white",
+    fontWeight: "bold",
+    textAlign:"center",
+    fontSize:22,
+  },
+  intro:{
+    flex:2,
+    color:"white",
+    textAlign: "center",
+    marginLeft:"auto",
+    marginRight:"auto",
+    fontSize:16,
+  },
+  contact:{
+    flex:1,
+    backgroundColor:"white",
+    alignSelf: "flex-start",
+    padding:10,
+    borderRadius:10,
+    marginLeft:"auto",
+    marginRight:"auto",
+  },
+  soustitre:{
+    flex:1,
+    color:"white",
+    fontSize: 18,
+    fontWeight:"bold",
+  },
+  dev:{
+    // flex:1,
+    display:"flex",
+    alignSelf: "flex-start",
+  },
+  devinfo:{
+    // flex:1,
+    color:"white",
+  },
+  reseau:{
+    // flex:1,
+    // color:"white",
+    width:90,
+    height:90,
+    backgroundColor:"white",
+    borderRadius:50,
+  },
+  lien:{
+    flex:1,
+  }
+  // allDev:{
+  //   flex:1,
+  //   flexDirection:"column"
+  // },
+
     
 })
 
