76f40679fb57045352390825e53aec16ffd78980
Esteves Tania <79203193+es-tania@users.noreply.github.com>
2022-03-17 09:07:13 +0100

    Accueil (#4)
    * accueil: création component accueil
    
    * feat(accueil.jsx, app.jsx)

--- App.jsx
+++ App.jsx
@@ -1,17 +1,38 @@
-import { StatusBar } from "expo-status-bar";
-import { View } from "react-native";
+import React from "react";
+
+import {Text, View, Image  } from "react-native";
 import { SafeAreaView,StyleSheet,Dimensions } from "react-native";
-import Header from "./components/header";
-import Rdv from "./page/rdv";
+import { StatusBar } from "expo-status-bar";
 import {NavigationContainer} from '@react-navigation/native';
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
+
+import Constants from "expo-constants";
+import Header from "./components/header";
+import Rdv from "./page/rdv";
 import Accueil from "./components/accueil";
+
+// import { StatusBar } from "expo-status-bar";
+// import Header from "./components/header";
+// import rdv from "./page/rdv";
+// import {NavigationContainer} from './react-navigation/native'; 
+// import {createNativeStackNavigator} from './react-navigation/native-stack';
 
 const Stack = createNativeStackNavigator();
 export default function App() {
- 
+  // const Stack = createNativeStackNavigator();
   return (
-    <NavigationContainer>
+    <View style={styles.container}>
+      {/* <Header/> */}
+      <Accueil style={styles.accueil}/>
+    </View>
+  
+    // <NavigationContainer>
+    //   <Stack.Navigator>
+    //     <Stack.Screen name="accueil" component={accueil}/>
+    //   </Stack.Navigator>
+    // </NavigationContainer>
+
+<NavigationContainer>
       <Stack.Navigator>
         <Stack.Screen name="accueil" component={Accueil}/>
         <Stack.Screen name="rendez-vous" component={Rdv}/>
@@ -19,17 +40,18 @@ export default function App() {
     </NavigationContainer>
   );
 }
+
 const globalColor = "#111111";
 const windowW = Dimensions.get("window").width;
 const windowH = Dimensions.get("window").height;
-const styles = StyleSheet.create({
 
+const styles = StyleSheet.create({
   container: {
     flex: 1,
-    alignItems: "center",
-    justifyContent: "center",
-    backgroundColor: "white",
-    width: windowW,
-  },
- 
-});
+    justifyContent: 'center',
+    alignItems:'center',
+    borderWidth: 1,
+  }
+})
+
+
